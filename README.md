14337a29c3a1981160127313b3e6a1da4fe6a4f6
cdorrat <cdorrat@gmail.com>
2011-10-26 08:22:05 +1100

    moved api docs to subdir

---
+++ README.md
@@ -0,0 +1,140 @@
+# reduce-fsm
+
+## Features
+reduce-fsm provides a simple way to specify clojure [finite state machines](http://en.wikipedia.org/wiki/Finite-state_machine), it allows you to:
+
+- Define define state machines that accumulate values (in the same was that reduce does)
+- Create lazy sequences from state machines
+- Perform stateful filtering with clojures filter/remove functions
+- Visualize the resulting state machines with graphviz
+
+All generated state machines are plain clojure functions and read events from clojure sequences.
+Events are dispatched with core.match and allow the use of all match features (guards, destructuring, regex matching, etc.)
+
+## Documentation and Source
+
+- This documentations is available at http://cdorrat.github.com/reduce-fsm/
+- The API documentation is available on github at http://cdorrat.github.com/reduce-fsm/api/index.html
+- The source is available on GitHub at https://github.com/cdorrat/reduce-fsm
+
+
+## Usage
+The fastest way to use this library is with Leiningen or Cake. Add the following to your project.clj dependencies:
+
+```clojure
+[cdorrat/reduce-fsm "0.1.0-SNAPSHOT"]
+```
+
+Use via:
+
+```clojure
+(require '[reduce-fsm :as fsm])
+```
+
+## Examples
+
+#### Basic FSM 
+The following example counts the number of times "ab" occurs in a sequence. 
+
+```clojure
+(defn inc-val [val & _] (inc val))
+
+(fsm/defsm count-ab
+  [[:start
+    \a -> :found-a]
+   [:found-a
+    \a ->  :found-a
+    \b -> {:action inc-val} :start
+    _ -> :start]])
+
+;; We can use the generated fsm like any function
+(map (partial count-ab 0) ["abaaabc" "aaacb" "bbbcab"])
+;; returns => (2 0 1)
+
+(show-fsm count-ab)
+;; displays the fsm diagram below
+
+```
+
+![show-fsm output](http://cdorrat.github.com/reduce-fsm/images/fsm-count-ab.png)
+
+
+#### Generating Lazy Sequences
+
+The fsm-seq functions return lazy sequences of values created by the emit function when a state change occurs. 
+This example looks for log lines where the sequence of events was (a,c) instead of the expected (a,b,c) and
+adds the unexpected event to the output sequence. 
+
+
+```clojure
+(defn emit-evt [val evt] evt)
+
+(defsm-seq log-search
+  [[:start
+    #".*event a" -> :found-a]
+   [:found-a
+    #".*event b" -> :found-b
+    #".*event c" -> {:emit emit-evt} :start]
+   [:found-b
+    #".*event c" -> :start]])
+
+;; The resulting function accepts a sequence of events 
+;; and returns a lazy sequence of emitted values
+(take 2 (log-search (cycle ["1 event a"
+                            "2 event b"
+                            "3 event c"
+                            "another event"
+                            "4 event a"
+                            "event x"
+                            "5 event c"])))
+
+;; returns => ("5 event c" "5 event c")
+
+(show-fsm log-search)
+;; displays the image below
+
+```
+
+![show-fsm output](http://cdorrat.github.com/reduce-fsm/images/fsm-log-search.png)
+
+
+#### Stateful Filtering
+States in filters are defined as passing values (default) or suppressing them {:pass false}.
+For each event the filter will return the pass value of the state it is in after processing the event (input sequence element).
+
+The following example suppresses values from the time a 3 is encountered until we see a 6.
+
+```clojure
+(defsm-filter sample-filter
+  [[:initial
+    3 -> :suppressing]
+   [:suppressing {:pass false}
+    6 -> :initial]])
+
+;; The resulting fsm is used with the clojure.core/filter and remove functions like this.
+(filter (sample-filter) [1 2 3 4 5 1 2 6 1 2])
+;; returns => (1 2 6 1 2)
+
+(show-fsm sample-filter)
+;; displays the diagram below
+```
+
+![show-fsm output](http://cdorrat.github.com/reduce-fsm/images/fsm-sample-filter.png)
+
+
+#### Other examples
+
+There are additional exmaples on [github](https://github.com/cdorrat/reduce-fsm/tree/master)
+in the examples and test directories  including:
+
+- a simple tcp server
+- matching repeating groups
+- using the :event-and-acc match syntax
+- using guards on events
+
+
+## License
+
+Copyright (C) 2011 Cameron Dorrat
+
+Distributed under the Eclipse Public License, the same as Clojure.
--- api/index.clj
+++ api/index.clj
@@ -1,6 +1,8 @@
 {:namespaces
- ({:source-url nil,
-   :wiki-url "reduce-fsm-api.html",
+ ({:source-url
+   "https://github.com/cdorrat/reduce-fsm/b55ff99cb0510158e838ad9159bb25c64d5aab43/src/reduce_fsm.clj",
+   :wiki-url
+   "http://cdorrat.github.com/reduce-fsm/reduce-fsm-api.html",
    :name "reduce-fsm",
    :doc
    "Generate and display functional finite state machines that accumumlate state\nin the same way as reduce.\nThis package allows you to:\n - Create basic fsm's (see fsm)\n - Create lazy sequences from state machines (see fsm-seq)\n - Create stateful filter functions for use with filter/remove (see fsm-filter)\n - Visualise state machines as"}),
@@ -8,20 +10,54 @@
  ({:arglists ([fsm-name states & opts]),
    :name "defsm",
    :namespace "reduce-fsm",
-   :source-url nil,
-   :raw-source-url nil,
-   :wiki-url "/reduce-fsm-api.html#reduce-fsm/defsm",
+   :source-url
+   "https://github.com/cdorrat/reduce-fsm/b55ff99cb0510158e838ad9159bb25c64d5aab43/src/reduce_fsm.clj#L289",
+   :raw-source-url
+   "https://github.com/cdorrat/reduce-fsm/b55ff99cb0510158e838ad9159bb25c64d5aab43/src/reduce_fsm.clj",
+   :wiki-url
+   "http://cdorrat.github.com/reduce-fsm//reduce-fsm-api.html#reduce-fsm/defsm",
    :doc
    "A convenience macro to define a fsm, equivalent to (def fsm-name (fsm states opts)\nsee reduce-fsm/fsm for details",
    :var-type "macro",
    :line 289,
    :file "/home/cnd/src/clojure/reduce-fsm/src/reduce_fsm.clj"}
+  {:arglists ([name states & fsm-opts]),
+   :name "defsm-filter",
+   :namespace "reduce-fsm",
+   :source-url
+   "https://github.com/cdorrat/reduce-fsm/b55ff99cb0510158e838ad9159bb25c64d5aab43/src/reduce_fsm.clj#L419",
+   :raw-source-url
+   "https://github.com/cdorrat/reduce-fsm/b55ff99cb0510158e838ad9159bb25c64d5aab43/src/reduce_fsm.clj",
+   :wiki-url
+   "http://cdorrat.github.com/reduce-fsm//reduce-fsm-api.html#reduce-fsm/defsm-filter",
+   :doc
+   "A convenience macro to define an fsm filter, equivalent to (def fsm-name (fsm-filter states opts)\nsee reduce-fsm/fsm-filter for details",
+   :var-type "macro",
+   :line 419,
+   :file "/home/cnd/src/clojure/reduce-fsm/src/reduce_fsm.clj"}
+  {:arglists ([name states & fsm-opts]),
+   :name "defsm-seq",
+   :namespace "reduce-fsm",
+   :source-url
+   "https://github.com/cdorrat/reduce-fsm/b55ff99cb0510158e838ad9159bb25c64d5aab43/src/reduce_fsm.clj#L585",
+   :raw-source-url
+   "https://github.com/cdorrat/reduce-fsm/b55ff99cb0510158e838ad9159bb25c64d5aab43/src/reduce_fsm.clj",
+   :wiki-url
+   "http://cdorrat.github.com/reduce-fsm//reduce-fsm-api.html#reduce-fsm/defsm-seq",
+   :doc
+   "A convenience macro to define an fsm sequence, equivalent to (def fsm-name (fsm-seq states opts)\nsee reduce-fsm/fsm-seq for details",
+   :var-type "macro",
+   :line 585,
+   :file "/home/cnd/src/clojure/reduce-fsm/src/reduce_fsm.clj"}
   {:arglists ([states & fsm-opts]),
    :name "fsm",
    :namespace "reduce-fsm",
-   :source-url nil,
-   :raw-source-url nil,
-   :wiki-url "/reduce-fsm-api.html#reduce-fsm/fsm",
+   :source-url
+   "https://github.com/cdorrat/reduce-fsm/b55ff99cb0510158e838ad9159bb25c64d5aab43/src/reduce_fsm.clj#L230",
+   :raw-source-url
+   "https://github.com/cdorrat/reduce-fsm/b55ff99cb0510158e838ad9159bb25c64d5aab43/src/reduce_fsm.clj",
+   :wiki-url
+   "http://cdorrat.github.com/reduce-fsm//reduce-fsm-api.html#reduce-fsm/fsm",
    :doc
    "Returns an fsm function that reads a sequence of events and returns\nan accumulated value (like reduce). The returned function will have the following 2 arities:\n [events]     - accepts a sequence of events\n [val events] - accepts an initial value for the accumulator and a sequence of events.\n\nThe generated function will return when one of the following is true:\n - There are no more events in the event sequence\n - The fsm reaches a terminal state\n - The fsm reaches a state defined by a function and it returns a truthy value\n\nParmaters:\n fsm      - the fsm definition (see below for syntax)\n fsm-opts - the following options are recognised:\n  :default-acc val - sets the initial value for the accumulator in the single arity version of the function\n  :dispatch - changes the way events are matched, the follow options are accepted:\n    - :event-only (default) - events are matched using the  core.match/match-1 syntax against the event only\n    - :event-and-acc        - events use the default match syntax and are matched against [acc-value event]\n\nFSM definitions:\n fsm's are defined as follows:\n [[state {:is-terminal true/false}?\n   event -> {:action a-fn}? target-state\n   event2 -> ...]\n  [target-state ...]]\n\nWhere\n state  - is a keyword or function\n state  - options (:is-terminal) are optional\n event  - is any legal core.match pattern (see https://github.com/clojure/core.match)\n action - is optional but must be a function if specified and the return value\n          will be used as the new accumulated state.\n\nState and Event Functions:\n State functions are called like so (state-fn acc) where acc is the current accumulated state.\n Event functions are called with (event-fn acc event from-state to-state) where\n   acc        - is the current accumulated state\n   event      - is the event that fired the transition\n   from-state - the state we're transitionin from\n   to-state   - the state we're transitioning to\n\nSee https://github.com/cdorrat/reduce-fsm for examples and documentation",
    :var-type "macro",
@@ -30,9 +66,12 @@
   {:arglists ([states & fsm-opts]),
    :name "fsm-filter",
    :namespace "reduce-fsm",
-   :source-url nil,
-   :raw-source-url nil,
-   :wiki-url "/reduce-fsm-api.html#reduce-fsm/fsm-filter",
+   :source-url
+   "https://github.com/cdorrat/reduce-fsm/b55ff99cb0510158e838ad9159bb25c64d5aab43/src/reduce_fsm.clj#L356",
+   :raw-source-url
+   "https://github.com/cdorrat/reduce-fsm/b55ff99cb0510158e838ad9159bb25c64d5aab43/src/reduce_fsm.clj",
+   :wiki-url
+   "http://cdorrat.github.com/reduce-fsm//reduce-fsm-api.html#reduce-fsm/fsm-filter",
    :doc
    "Returns a function that returns fsm filters suitable for use with clojure.core/filter and remove.\nEach state in the fsm definition has a :pass attribute that will be returned from the generated function\nwhen it is in that state.\n\nThe returned function will have the following 2 arities:\n []    - creates a filter with the default accumulator state (nil or the value from :default-acc)\n [val] - accepts an initial value for the accumulator \n\nParmaters:\nfsm      - the fsm definition (see below for syntax)\nfsm-opts - the following options are recognised:\n  :default-acc val - sets the initial value for the accumulator in the single arity version function\n  :dispatch - changes the way events are matched, the follow options are accepted:\n    - :event-only (default) - events are matched using the  core.match/match-1 syntax against the event only\n    - :event-and-acc        - events use the default match syntax and are matched against [acc-value event]\n\nFSM definitions:\nfilters's are defined as follows:\n [[state {:pass true/false}?\n   event -> {:action a-fn}? target-state]\n  [target-state ...]]\n\nWhere\n  state is a keyword \n  state option (:pass) is optional, it defaults to true\n  event is any legal core.match pattern (see https://github.com/clojure/core.match)\n  action is optional but must be a function if specified and their return value\n         will be used as the new accumulated state\n\nEvent functions are called with (event-fn acc event from-state to-state) where\n  acc        - is the current accumulated state\n  event      - is the event that fired the transition\n  from-state - the state we're transitionin from\n  to-state   - the state we're transitioning to\n\nExample:\n  Suppress numbers after seeing a 3 until we see a 6.\n\n  (def f (fsm-filter [[:initial \n \t               3 -> :suppressing]\n \t              [:suppressing {:pass false}\n \t               6 -> :initial]]))\n\n  (= [1 2 6 1 2] (filter (f) [1 2 3 4 5 1 2 6 1 2]))",
    :var-type "macro",
@@ -41,29 +80,38 @@
   {:arglists ([states & fsm-opts]),
    :name "fsm-seq",
    :namespace "reduce-fsm",
-   :source-url nil,
-   :raw-source-url nil,
-   :wiki-url "/reduce-fsm-api.html#reduce-fsm/fsm-seq",
+   :source-url
+   "https://github.com/cdorrat/reduce-fsm/b55ff99cb0510158e838ad9159bb25c64d5aab43/src/reduce_fsm.clj#L523",
+   :raw-source-url
+   "https://github.com/cdorrat/reduce-fsm/b55ff99cb0510158e838ad9159bb25c64d5aab43/src/reduce_fsm.clj",
+   :wiki-url
+   "http://cdorrat.github.com/reduce-fsm//reduce-fsm-api.html#reduce-fsm/fsm-seq",
    :doc
    "Returns an fsm function that produces lazy sequences from a finite state machine.\n The state machines can optionally add new values to the lazy sequence on transitions\n (with the :emit option) and may accumulate state in the same way as reduce.\n\nThe returned function will have the following 2 arities:\n [events]     - accepts a sequence of events\n [val events] - accepts an initial value for the accumulator and a sequence of events.\n\nThe generated function will produce a lazy seqnuence that ends when one of the following is true:\n - There are no more events in the event sequence\n - The fsm reaches a terminal state\n - The fsm reaches a state defined by a function and it returns a truthy value\n\nParmaters:\n fsm      - the fsm definition (see below for syntax)\n fsm-opts - the following options are recognised:\n  :default-acc val - sets the initial value for the acculator in the single arity version of the function\n  :dispatch - changes the way events are matched, the follow options are accepted:\n    - :event-only (default) - events are matched using the  core.match/match-1 syntax against the event only\n    - :event-and-acc        - events use the default match syntax and are matched against [acc-value event]\n\nFSM definitions:\n fsm's are defined as follows:\n [[state {:is-terminal true/false}?\n   event -> {:emit emit-fn :action action-fn}? target-state\n   event2 -> ...]\n  [target-state ...]]\n\nWhere\n state  is a keyword or function\n state options (:is-terminal) are optional\n event  is any legal core.match pattern (see https://github.com/clojure/core.match)\n emit   is optional but its value must be a function, the return value will be added to the lazy sequence\n action is optional but its value must be a function if specified and their return value\n        will be used as the new accumulated state\n\nState and Event Functions:\n State functions are called with the current accumulated statelike so (state-fn acc).\n Emit functions are called with  (emit-fn acc event),\n Action functions are called with (action-fn acc event from-state to-state) where\n   acc        - is the current accumulated state\n   event      - is the event that fired the transition\n   from-state - the state we're transitionin from\n   to-state   - the state we're transitioning to\n\nSee https://github.com/cdorrat/reduce-fsm for examples and documentation",
    :var-type "macro",
-   :line 520,
+   :line 523,
    :file "/home/cnd/src/clojure/reduce-fsm/src/reduce_fsm.clj"}
-  {:raw-source-url nil,
-   :source-url nil,
-   :wiki-url "/reduce-fsm-api.html#reduce-fsm/save-fsm-image",
+  {:raw-source-url
+   "https://github.com/cdorrat/reduce-fsm/b55ff99cb0510158e838ad9159bb25c64d5aab43/src/reduce_fsm.clj",
+   :source-url
+   "https://github.com/cdorrat/reduce-fsm/b55ff99cb0510158e838ad9159bb25c64d5aab43/src/reduce_fsm.clj#L606",
+   :wiki-url
+   "http://cdorrat.github.com/reduce-fsm//reduce-fsm-api.html#reduce-fsm/save-fsm-image",
    :namespace "reduce-fsm",
-   :line 600,
+   :line 606,
    :file "/home/cnd/src/clojure/reduce-fsm/src/reduce_fsm.clj",
    :var-type "multimethod",
    :doc
    "Save the state transition diagram for an fsm as a png.\nExpects the following parameters:\n  - fsm      - the fsm to render\n  - filename - the output file for the png.",
    :name "save-fsm-image"}
-  {:raw-source-url nil,
-   :source-url nil,
-   :wiki-url "/reduce-fsm-api.html#reduce-fsm/show-fsm",
+  {:raw-source-url
+   "https://github.com/cdorrat/reduce-fsm/b55ff99cb0510158e838ad9159bb25c64d5aab43/src/reduce_fsm.clj",
+   :source-url
+   "https://github.com/cdorrat/reduce-fsm/b55ff99cb0510158e838ad9159bb25c64d5aab43/src/reduce_fsm.clj#L605",
+   :wiki-url
+   "http://cdorrat.github.com/reduce-fsm//reduce-fsm-api.html#reduce-fsm/show-fsm",
    :namespace "reduce-fsm",
-   :line 599,
+   :line 605,
    :file "/home/cnd/src/clojure/reduce-fsm/src/reduce_fsm.clj",
    :var-type "multimethod",
    :doc

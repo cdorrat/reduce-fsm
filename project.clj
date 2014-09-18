987204ef722101effc97ebe944c11772fa09c5e1
Cameron Dorrat <cdorrat@gmail.com>
2014-09-18 20:56:58 +1000

    updated dorothy version to 0.6.0

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject reduce-fsm "0.1.1"
   :description "Clojure finite state machines"
   :dependencies [[org.clojure/clojure "1.6.0"]
-		 [dorothy "0.0.5"]
+		 [dorothy "0.0.6"]
                  [org.clojure/core.match "0.2.1"]]
   :dev-dependencies [[server-socket "1.0.0"] ;; used for examples/simple_server.clj		     
 		     [org.clojars.weavejester/autodoc "0.9.0"]]
